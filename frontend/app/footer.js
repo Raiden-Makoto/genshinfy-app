a5428e8695370f8ea343df0801420d8a03706a5b
Max <mc.cui@mail.utoronto.ca>
2025-03-17 00:26:27 -0400

    footer centered

--- frontend/app/footer.js
+++ frontend/app/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="text-purple-300 text-bottom py-6 font-[GenshinImpact]">
+        <footer className="text-center text-purple-300 text-bottom py-6 font-[GenshinImpact]">
             &copy; {new Date().getFullYear()} Genshinfy.App. All rights reserved. <br /> Images sourced from HoYoverse Universe Index. <br /> Not affiliated with HoYoVerse.
         </footer>
     );
